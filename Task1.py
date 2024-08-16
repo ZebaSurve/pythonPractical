a3c25a0b6f80e7ee3eadc6b366957d1cb89b269a
ZebaSurve <zeba2494@gmail.com>
2024-08-16 14:28:05 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter number here:"))
+num2 = int(input("Enter number here:"))
+
+print(f"The max number from num1 and num2 is: {max(num1,num2)}")
+print(f"The power of num1 is: {pow(num1,num2)}")
+print(f"The mul of  num1 and num2 is: {(num1*num2)}")
+print(f"The div is: {(num1/num2)}")
+print(f"The sub is: {(num1-num2)}")
+print(f"The sum is:{(num1+num2)}")
