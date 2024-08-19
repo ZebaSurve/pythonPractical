63531e38b41ceb39a3aa27afb4ec30ddc64d5871
ZebaSurve <zeba2494@gmail.com>
2024-08-19 22:52:38 +0530

    Add files via upload

---
+++ Task 3.py
@@ -0,0 +1,28 @@
+#Operator = is which is use to store right value to the left value ref.
+
+age = 30
+print(age)
+
+name = "ZEBA"
+print(name)
+
+# Operator == is campare the right and left value in Tue or False
+
+x=12
+y=22
+print(x==y)
+
+x=20
+y=20
+print(y==x)
+
+#What does the ** operator do in Python, and how is it used?
+
+# ** is power
+x=2
+y=3
+print(x**y)
+
+print(4**3)
+
+#What does the ^ operator do in Python, and in what context is it commonly used?
---
+++ Task6.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the number: \n"))
+
+print("Square of the", num, "is", num**2)
+print("Cube of the", num, "is", num**3)
+
+# Alternatively we can use Pow function
+
+print("Square of the", num, "using pow function is", pow(num,2))
+print("Square of the", num, "using pow function is", pow(num,3))
